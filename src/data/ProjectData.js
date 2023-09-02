194d1af28ff82dcf099f348d5eb9a0c4ecdabd53
BhartSingh <monuranoliya731@gmail.com>
2023-09-02 09:42:51 +0530

    Initial commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,86 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/Deno.png",
+    title: "Deno",
+    description: "Frontend mentor challenge, Deno is an interactive form.",
+    tech_stack: "Html, Css, Javascript",
+    github_url: "https://bhartsingh.github.io/Interactive-card-details/",
+    demo_url:
+      "https://64eb3473e5e53c5135cb6f93--storied-cassata-9276d4.netlify.app/",
+  },
+  {
+    img: "/Zomato.png",
+    title: "Zomato clone",
+    description:
+      "This is a Zomato clone app built using React, Redux, Tailwind CSS, and React Router DOM.",
+    tech_stack: "React js ,Redux, Tailwind Css",
+    github_url: "https://github.com/BhartSingh/Zomato_App",
+    demo_url: "https://majestic-crisp-2a478d.netlify.app/",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77205201/252886480-e39531c9-07ea-427a-980b-4c4630c0f198.png",
+    title: "Weather App",
+    description: "Weather App that fetches real time weather data",
+    tech_stack: "Html, Css, Javascript",
+    github_url: "https://bhartsingh.github.io/Weather-app/",
+    demo_url:
+      "https://64eb35368de23f5a5529b436--tubular-duckanoo-6ca76f.netlify.app/",
+  },
+  {
+    img: "/FoodZone.png",
+    title: "FoodZone Website ",
+    description:
+      "This is a FoodZone clone website built using React, Redux, Tailwind CSS, and React Router DOM.",
+    tech_stack: "React js ,Redux, Css",
+    github_url: "https://github.com/BhartSingh/FoodZone-Website",
+    demo_url: "https://frolicking-quokka-de81d1.netlify.app/",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/77205201/252889883-4d61922c-1362-4554-8035-049b42af1514.png",
+    title: "Redesign Youtube",
+    description:
+      "Created Youtube using react and firebase that provides authentication , like and subscribe videos features ",
+    tech_stack: "Reactjs, Css, Firebase",
+    github_url: "https://github.com/BhartSingh/Redesign_Youtube",
+    demo_url:
+      "https://64ed38413bec1c1127e36748--delicate-semifreddo-dabdc1.netlify.app/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://camo.githubusercontent.com/2519c0d24b4bb0eb1c72f2c3281b7283a67107c812976a2fe4177770b47e7a5b/68747470733a2f2f63646e342e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6c6f676f732d332f3630302f52656163742e6a735f6c6f676f2d313032342e706e67",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://camo.githubusercontent.com/5734d0669fe22ce04a1cb989a156cd32c379875f6bca56d5210c9432824856d9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667",
+    name: "Tailwind Css",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive",
+  },
+];
